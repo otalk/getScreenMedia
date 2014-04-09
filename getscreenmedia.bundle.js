437f30278decb7ecffc05b1974dda9c7974e8516
Philipp Hancke <fippo@andyet.net>
2014-04-09 16:53:47 +0200

    chromeMediaSource desktop support that appears to be rolling out in M34

--- getscreenmedia.bundle.js
+++ getscreenmedia.bundle.js
@@ -2,6 +2,9 @@
 // getScreenMedia helper by @HenrikJoreteg
 var getUserMedia = require('getusermedia');
 
+// cache for constraints and callback
+var cache = {};
+
 module.exports = function (constraints, cb) {
     var hasConstraints = arguments.length === 2;
     var callback = hasConstraints ? cb : constraints;
@@ -13,18 +16,62 @@ module.exports = function (constraints, cb) {
         return callback(error);
     }
 
-    constraints = (hasConstraints && constraints) || { 
-        video: {
-            mandatory: {
+    if (window.navigator.userAgent.match('Chrome')) { 
+        var chromever = parseInt(window.navigator.userAgent.match(/Chrome\/(.*) /)[1], 10);
+        if (chromever >= 26 && chromever <= 33) {
+            // chrome 26 - chrome 33 way to do it -- requires bad chrome://flags
+            constraints = (hasConstraints && constraints) || { 
+                video: {
+                    mandatory: {
+                        maxWidth: window.screen.width,
+                        maxHeight: window.screen.height,
+                        maxFrameRate: 3,
+                        chromeMediaSource: 'screen'
+                    }
+                }
+            };
+            getUserMedia(constraints, callback);
+        } else {
+            // chrome 34+ way requiring an extension
+            var pending = window.setTimeout(function () {
+                error = new Error('NavigatorUserMediaError');
+                error.name = 'EXTENSION_UNAVAILABLE';
+                return callback(error);
+            }, 1000);
+            cache[pending] = [callback, hasConstraints ? constraint : null];
+            window.postMessage({ type: 'getScreen', id: pending }, '*');
+        }
+    }
+};
+
+window.addEventListener('message', function (event) { 
+    if (event.origin != window.location.origin) {
+        return;
+    }
+    if (event.data.type == 'gotScreen' && cache[event.data.id]) {
+        var data = cache[event.data.id];
+        var constraints = data[1];
+        var callback = data[0];
+        delete cache[event.data.id];
+
+        if (event.data.sourceId === '') { // user canceled
+            var error = error = new Error('NavigatorUserMediaError');
+            error.name = 'PERMISSION_DENIED';
+            callback(error);
+        } else {
+            constraints = constraints || {audio: false, video: {mandatory: {
+                chromeMediaSource: 'desktop', 
+                chromeMediaSourceId: event.data.sourceId,
                 maxWidth: window.screen.width,
                 maxHeight: window.screen.height,
                 maxFrameRate: 3,
-                chromeMediaSource: 'screen'
-            }
+            }}};
+            getUserMedia(constraints, callback);
         }
-    };
-    getUserMedia(constraints, callback);
-};
+    } else if (event.data.type == 'getScreenPending') {
+        window.clearTimeout(event.data.id);
+    }
+});
 
 },{"getusermedia":2}],2:[function(require,module,exports){
 // getUserMedia helper by @HenrikJoreteg
