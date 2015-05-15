3a46fe9f74df2de1cc4e69ff8d8b660abcb3bb82
Xander Dumaine <xander.dumaine@inin.com>
2015-05-14 23:16:45 -0700

    Update background.js to respond to new method messages

--- chrome-extension-sample/background.js
+++ chrome-extension-sample/background.js
@@ -1,25 +1,20 @@
 /* background page, responsible for actually choosing media */
-chrome.runtime.onConnect.addListener(function (channel) {
-    channel.onMessage.addListener(function (message) {
-        switch(message.type) {
+chrome.runtime.onMessageExternal.addListener(function (message, sender, callback) {
+    switch(message.type) {
         case 'getScreen':
-            var pending = chrome.desktopCapture.chooseDesktopMedia(message.options || ['screen', 'window'], 
-                                                                   channel.sender.tab, function (streamid) {
+            var pending = chrome.desktopCapture.chooseDesktopMedia(message.options || ['screen', 'window'],
+                                                                   sender.tab, function (streamid) {
                 // communicate this string to the app so it can call getUserMedia with it
                 message.type = 'gotScreen';
                 message.sourceId = streamid;
-                channel.postMessage(message);
+                callback(message);
+                return false;
             });
-            // let the app know that it can cancel the timeout
-            message.type = 'getScreenPending';
-            message.request = pending;
-            channel.postMessage(message);
-            break;
+            return true; // retain callback for chooseDesktopMedia result
         case 'cancelGetScreen':
             chrome.desktopCapture.cancelChooseDesktopMedia(message.request);
             message.type = 'canceledGetScreen';
-            channel.postMessage(message);
-            break;
-        }
-    });
+            callback(message);
+            return false; //dispose callback
+    }
 });
